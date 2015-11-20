62d56d1a5fe45b9a341f8bd71c6279a87a72a17b
Andy Walpole <me@andywalpole.me>
2015-11-20 12:32:55 +0000

    third and final test

--- test/test.js
+++ test/test.js
@@ -18,6 +18,8 @@ var jsonOne = JSON.parse(fs.readFileSync(absolutePath('data/data_one.json'), 'ut
 var pageTwo = fs.readFileSync(absolutePath('html/page_two.html'), 'utf8').toString();
 var pageTwoResult = fs.readFileSync(absolutePath('html/page_two_result.html'), 'utf8').toString();
 var jsonTwo = JSON.parse(fs.readFileSync(absolutePath('data/data_two.json'), 'utf8'));
+var pageThree = fs.readFileSync(absolutePath('html/page_three.html'), 'utf8').toString();
+var pageThreeResult = fs.readFileSync(absolutePath('html/page_three_result.html'), 'utf8').toString();
 
 function testOne(input, output, jsonFile, done) {
   posthtml()
@@ -51,7 +53,27 @@ function testTwo(input, output, jsonFile, done) {
   });
 }
 
-it('test one', function(done) {
+function testThree(input, output, jsonFile, done) {
+  posthtml()
+    .use(posthtmlHeadElements({headElements: jsonFile}))
+    .process(input)
+    .then(function(result) {
+
+      expect(result.html).to.not.have.string('<meta charset="utf-8">');
+      expect(result.html).to.not.have.string('<title>Web Starter Kit</title>');
+      expect(result.html).to.not.have.string('<link rel="manifest" href="manifest.json">');
+      expect(result.html).to.not.have.string('<meta name="mobile-web-app-capable" content="yes">');
+      expect(result.html).to.not.have.string('<link rel="apple-touch-icon" href="images/touch/apple-touch-icon.png">');
+      expect(result.html).to.not.have.string('<meta name="msapplication-TileImage" content="mages/touch/ms-touch-icon-144x144-precomposed.png">');
+      expect(result.html).to.not.have.string('<link rel="stylesheet" href="styles/main.css">');
+
+      done();
+    }).catch(function(error) {
+    done(error);
+  });
+}
+
+it('Test one: basic search and replace', function(done) {
   var input = '<posthtml-head-elements></posthtml-head-elements>';
   var output = [
     '<meta charset="utf-8">',
@@ -68,7 +90,11 @@ it('test one', function(done) {
   testOne(input, output, jsonOne, done);
 });
 
-it('test two', function(done) {
+it('Test two: complete search and replace', function(done) {
   testTwo(pageTwo, pageTwoResult, jsonTwo, done);
+});
+
+it('Test three: failed search and replace. HTML head document does not contain custom element, <posthtml-head-elements>', function(done) {
+  testThree(pageThree, pageThreeResult, jsonTwo, done);
 });
 
